585d42348cd7b8300c7aa3e4c8f5e5c1ca1dcb5b
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-02-23 23:08:33 +0100

    home/index view update

--- Models/FilterModel.cs
+++ Models/FilterModel.cs
@@ -2,5 +2,15 @@
 {
     public class FilterModel
     {
+
+        public string Search { get; set; }
+        public int? Tag { get; set; }
+        public float? MinEngineSize { get; set; }
+        public float? MaxEngineSize { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
     }
 }
