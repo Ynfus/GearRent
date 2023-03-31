784e8cad4c72fb7bdd03fb9491d9653393bf6d9c
Ynfus <95777380+Ynfus@users.noreply.github.com>
2023-04-01 00:02:15 +0200

    Contact site with contact form

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace GearRent.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string name, string email, string phone, string message)
+        {
+            //var mail = new MailMessage();
+            //mail.From = new MailAddress("");
+            //mail.To.Add("info@GearRent.pl");
+            //mail.Subject = "Wiadomość z formularza kontaktowego";
+            //mail.Body = $"Imię i nazwisko: {name}\nAdres e-mail: {email}\nNumer telefonu: {phone}\n\n{message}";
+
+            //var smtp = new SmtpClient();
+            //smtp.Host = "";
+            //smtp.Port = 587;
+            //smtp.Credentials = new System.Net.NetworkCredential("username", "password");
+            //smtp.EnableSsl = true;
+
+            //smtp.Send(mail);
+            TempData["messageSent"] = true;
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearRent.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message.")]
+        public string Message { get; set; }
+
+    }
+}
