4fafb328d95ee3e185a81b0a334b734f43f35cd7
Ynfus <bartekzakrz2001@gmail.com>
2023-06-24 14:46:57 +0200

    Create ReportViewModel.cs

---
+++ Models/ReportViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GearRent.Models
+{
+    public class ReportViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public int TotalReservations { get; set; }
+    }
+}
