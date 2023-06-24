9cf4c8e0db2aca4d4132c0f1ccfb08a78ffa7b18
Ynfus <bartekzakrz2001@gmail.com>
2023-06-24 14:49:58 +0200

    Create CarReservationsViewModel.cs

---
+++ Models/CarReservationsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GearRent.Models
+{
+    public class CarReservationsViewModel
+    {
+
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public int CarYear { get; set; }
+        public string UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
