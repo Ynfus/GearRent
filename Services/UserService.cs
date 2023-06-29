260f20053f44f6fd5357fb27f625da513fb31543
Ynfus <bartekzakrz2001@gmail.com>
2023-06-29 23:15:22 +0200

    Create UserService.cs

---
+++ Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using GearRent.Data;
+using GearRent.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearRent.Services
+{
+    public interface IUserService
+    {
+        Task<List<IdentityUser>> GetAllUsersAsync();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+
+
+        public async Task<List<IdentityUser>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
