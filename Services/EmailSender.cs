16da889e43d45957f511839bea44bdee0c849d83
Ynfus <bartekzakrz2001@gmail.com>
2023-06-29 23:14:58 +0200

    Create EmailSender.cs

---
+++ Services/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using System.Net;
+
+namespace GearRent.Services
+{
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger<EmailSender> _logger;
+
+        public EmailSender(ILogger<EmailSender> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            try
+            {
+                using (SmtpClient smtpClient = new SmtpClient("localhost", 2525))
+                {
+                    //smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = true;
+
+                    MailMessage mailMessage = new MailMessage();
+                    mailMessage.From = new MailAddress("sender@example.com");
+                    mailMessage.To.Add(new MailAddress(toEmail));
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = message;
+
+                    await smtpClient.SendMailAsync(mailMessage);
+
+                    _logger.LogInformation($"Email sent successfully to {toEmail}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Failed to send email to {toEmail}");
+                throw;
+            }
+        }
+    }
+}
