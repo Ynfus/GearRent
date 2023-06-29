fd154e6e20e251710e9533bb2e7af650cab267d6
Ynfus <bartekzakrz2001@gmail.com>
2023-06-29 23:15:06 +0200

    Create ReservationService.cs

---
+++ Services/ReservationService.cs
@@ -0,0 +1,44 @@
+﻿using GearRent.Data;
+using GearRent.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearRent.Services
+{
+    public interface IReservationService
+    {
+        Task<List<Reservation>> GetAllReservationsAsync();
+        Task<Reservation> GetReservationByIdAsync(int id);
+        Task<Reservation> GetReservationByIdAsyncInclude(int id);
+    }
+
+    public class ReservationService : IReservationService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Reservation>> GetAllReservationsAsync()
+        {
+            return await _context.Reservations
+                .Include(r => r.Car)
+                .Include(r => r.User)
+                .ToListAsync();
+        }
+
+        public async Task<Reservation> GetReservationByIdAsyncInclude(int id)
+        {
+            return await _context.Reservations
+                .Include(r => r.Car)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+        public async Task<Reservation> GetReservationByIdAsync(int id)
+        {
+            return await _context.Reservations
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
