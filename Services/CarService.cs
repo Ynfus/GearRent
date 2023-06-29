7a7f80b9adb5cf505de4076700e8da2afd0c08f1
Ynfus <bartekzakrz2001@gmail.com>
2023-06-29 23:15:13 +0200

    Create CarService.cs

---
+++ Services/CarService.cs
@@ -0,0 +1,32 @@
+﻿using GearRent.Data;
+using GearRent.Data.Migrations;
+using GearRent.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearRent.Services
+{
+    public interface ICarService
+    {
+        Task<Car> GetCarAsync(int id);
+
+    }
+
+    public class CarService : ICarService
+    {
+        private readonly ApplicationDbContext _context;
+        public CarService(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Car> GetCarAsync(int id)
+        {
+            return await _context.Cars.FindAsync(id);
+
+        }
+
+
+
+    }
+}
