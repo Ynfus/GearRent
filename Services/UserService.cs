286e4bcad49b175374ee558fe693765e6ecb7b82
Ynfus <bartekzakrz2001@gmail.com>
2023-07-01 18:00:50 +0200

    Update UserService.cs

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -8,17 +8,27 @@ namespace GearRent.Services
     public interface IUserService
     {
         Task<List<IdentityUser>> GetAllUsersAsync();
+        Task<string> GetUserEmailByIdAsync(string userId);
     }
 
     public class UserService : IUserService
     {
         private readonly ApplicationDbContext _context;
 
-
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public async Task<List<IdentityUser>> GetAllUsersAsync()
         {
             return await _context.Users.ToListAsync();
         }
+        public async Task<string> GetUserEmailByIdAsync(string userId)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            return user?.Email;
+        }
+
     }
 }
