235193ee31e01149539254f48e1c0ee8fda99cf6
Ynfus <bartekzakrz2001@gmail.com>
2023-08-19 12:34:44 +0200

    Update PaginatedList.cs

--- PaginatedList/PaginatedList.cs
+++ PaginatedList/PaginatedList.cs
@@ -19,10 +19,19 @@ namespace GearRent.PaginatedList
 
         public bool HasNextPage => PageIndex < TotalPages;
 
+        public static async Task<PaginatedList<T>> CreateAsync1(IQueryable<T> source, int pageIndex, int pageSize, bool includeTotalCount = false)
+        {
+            var count = includeTotalCount ? await source.CountAsync() : 0;
+            var totalPages = includeTotalCount ? (int)Math.Ceiling(count / (double)pageSize) : 0;
+
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, totalPages, pageIndex, pageSize);
+        }
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
