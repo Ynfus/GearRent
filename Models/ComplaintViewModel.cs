920ebcda1ca88faff4102e913f22e410f34c5f66
Ynfus <bartekzakrz2001@gmail.com>
2023-08-21 20:56:42 +0200

    Update

---
+++ Models/ComplaintViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearRent.Models
+{
+    public class ComplaintViewModel
+    {
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        [EmailAddress(ErrorMessage = "Niepoprawny format adresu e-mail.")]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string ReservationNumber { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string ComplaintDescription { get; set; }
+    }
+}
+
+
+
+
+
