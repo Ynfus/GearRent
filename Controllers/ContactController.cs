371ddf3b0a02b66aef054b9b04224dcbaa4cf407
Ynfus <bartekzakrz2001@gmail.com>
2023-08-21 20:56:58 +0200

    Update ContactController.cs

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -30,11 +30,42 @@ namespace GearRent.Controllers
         [HttpPost]
         public ActionResult Index(ContactViewModel contactViewModel)
         {
-            _emailSender.SendEmailAsync(contactViewModel.Email,"Wiadomość z formularza kontaktowego "+ DateTime.Now.ToString() ,contactViewModel.Message+"\n"+contactViewModel.Name);
+            _emailSender.SendEmailAsync(contactViewModel.Email, "Wiadomość z formularza kontaktowego " + DateTime.Now.ToString(), contactViewModel.Message + "\n" + contactViewModel.Name);
             TempData["messageSent"] = true;
 
             return RedirectToAction("Index");
         }
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        public IActionResult DownloadFile(string fileName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\pdf", fileName);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+            string fileExtension = Path.GetExtension(filePath);
+            string contentType = "application/pdf";
+            return File(System.IO.File.OpenRead(filePath), contentType, fileName);
+        }
+        public IActionResult OWNP()
+        {
+            return View();
+        }
+        public IActionResult Complaint()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Complaint(ComplaintViewModel complaintViewModel)
+         {
+            _emailSender.SendEmailAsync(complaintViewModel.Email, "Wiadomość z formularza reklamacyjnego zamówienia nr: " + complaintViewModel.ReservationNumber + "  " + DateTime.Now.ToString(),
+                complaintViewModel.ComplaintDescription + "\n\nZłozone przez: " + complaintViewModel.FullName + "\nNumer kontaktowy: " + complaintViewModel.Phone);
+            TempData["messageSent"] = true;
 
+            return RedirectToAction("Complaint");
+        }
     }
 }
