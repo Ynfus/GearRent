bd7e238853c7508922356d0323f8dbb860b27d0a
Ynfus <bartekzakrz2001@gmail.com>
2023-09-01 22:48:58 +0200

    Update email message

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -24,12 +24,23 @@ namespace GearRent.Services
                     //smtpClient.EnableSsl = true;
                     smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
                     smtpClient.UseDefaultCredentials = true;
-
+                    string fullMessage = "Witaj!\n\n";
+                    fullMessage += message;
+                    fullMessage += "\n\nDziękujemy za korzystanie z naszych usług!\n";
+                    fullMessage += "Pozdrawiamy,\nZespół GearRent";
+                    fullMessage += "\n\n\nDane kontaktowe:\n";
+                    fullMessage += "ul. Półwiejska 42, 61-888 Poznań\n";
+                    fullMessage += "+48 123 456 789\n";
+                    fullMessage += "kontakt@gearrent.pl\n\n";
+                    fullMessage += "Godziny otwarcia:\n";
+                    fullMessage += "Poniedziałek - piątek: 8:00 - 20:00\n";
+                    fullMessage += "Sobota: 9:00 - 18:00\n";
+                    fullMessage += "Niedziela: nieczynne";
                     MailMessage mailMessage = new MailMessage();
                     mailMessage.From = new MailAddress("sender@example.com");
                     mailMessage.To.Add(new MailAddress(toEmail));
                     mailMessage.Subject = subject;
-                    mailMessage.Body = message;
+                    mailMessage.Body = fullMessage;
 
                     await smtpClient.SendMailAsync(mailMessage);
 
