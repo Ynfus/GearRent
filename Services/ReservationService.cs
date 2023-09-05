9fa0490c2cb95853dada0885179c4d94415f88c8
Ynfus <bartekzakrz2001@gmail.com>
2023-09-05 23:22:05 +0200

    update task with unpaid status for background job

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -1,5 +1,6 @@
 ﻿using GearRent.Data;
 using GearRent.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace GearRent.Services
@@ -9,16 +10,20 @@ namespace GearRent.Services
         Task<List<Reservation>> GetAllReservationsAsync();
         Task<Reservation> GetReservationByIdAsync(int id);
         Task<Reservation> GetReservationByIdAsyncInclude(int id);
-        Task<List<string>> CancelUnpaidReservationsAndSendEmailsAsync();
+        Task CancelUnpaidReservationsAndSendEmailsAsync(int id);
     }
 
     public class ReservationService : IReservationService
     {
         private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly ICarService _carService;
 
-        public ReservationService(ApplicationDbContext context)
+        public ReservationService(ApplicationDbContext context, IEmailSender emailSender, ICarService carService)
         {
             _context = context;
+            _emailSender = emailSender;
+            _carService = carService;
         }
 
         public async Task<List<Reservation>> GetAllReservationsAsync()
@@ -41,28 +46,41 @@ namespace GearRent.Services
             return await _context.Reservations
                 .FirstOrDefaultAsync(r => r.Id == id);
         }
-        public async Task<List<string>> CancelUnpaidReservationsAndSendEmailsAsync()
+
+        public async Task CancelUnpaidReservationsAndSendEmailsAsync(int reservationId)
         {
-            var userEmails = new List<string>();
-            var reservationsToCancel = await _context.Reservations
-                .Include(r => r.Car)
-                .Include(r => r.User)
-                .Where(r => r.Status == ReservationStatus.Unpaid && r.StartDate < DateTime.Now.AddDays(3))
-                .ToListAsync();
+            var reservation = await _context.Reservations.FindAsync(reservationId);
 
-            foreach (var reservation in reservationsToCancel)
+            if (reservation == null)
             {
-                reservation.Status = ReservationStatus.Canceled;
-
-                _context.Entry(reservation).State = EntityState.Modified;
-
-                userEmails.Add(reservation.User.Email);
+                return;
             }
 
+            if (reservation.Status != ReservationStatus.Unpaid)
+            {
+                return;
+            }
+
+            reservation.Status = ReservationStatus.Canceled;
+            _context.Update(reservation);
             await _context.SaveChangesAsync();
 
+            try
+            {
+                Car car = await _carService.GetCarAsync(reservation.CarId);
+                var email = await _context.Users
+                    .Where(u => u.Id == reservation.UserId)
+                    .Select(u => u.Email)
+                    .FirstOrDefaultAsync();
+                var subject = $"Anulacja rezerwacji {reservation.Id}";
+                var body = $"Twoja rezerwacja na {car.Make} {car.Model} została anulowana ze względu na nieopłacenie jej 3 dni przed startem wypożyczenia.\nSkontaktuj się z nami mailowo w sprawie zwrotu środków";
+                await _emailSender.SendEmailAsync(email, subject, body);
+            }
+            catch (Exception ex)
+            {
 
-            return userEmails;
+            }
         }
     }
 }
+
