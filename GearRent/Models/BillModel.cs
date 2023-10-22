8f3743cfa5000a02529b0b8281bf4ccb3f8e7e5b
Ynfus <bartekzakrz2001@gmail.com>
2023-10-22 22:20:57 +0200

    Create BillModel.cs

---
+++ GearRent/Models/BillModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GearRent.Models
+{
+    public class BillModel
+    {
+        public int BillId { get; set; }
+        public string Name { get; set; }
+        public decimal TotalValue { get; set; }
+        public int Days { get; set; }
+        public string Date { get; set; }
+        public string CarModel { get; set; }
+    }
+}
